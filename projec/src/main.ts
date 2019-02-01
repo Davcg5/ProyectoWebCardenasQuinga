72baa4fcfb83c7eb002ddbc458435d7b6918aa69
davini10c <vinicio_520@hotmail.com>
2019-02-01 12:39:39 -0500

    sesion archivo plano

--- projec/src/main.ts
+++ projec/src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import * as express from 'express';
 import * as session from 'express-session';
+var FileStore = require('session-file-store')(session);
 
 //var session = require('express-session');
 async function bootstrap() {
@@ -10,10 +11,14 @@ async function bootstrap() {
     app.set('views', __dirname + '/views');
     app.use(
         session({
+            name: 'server-session-id',
             secret: 'epn ingenieria en sitemasSS',
             resave: false,
             saveUninitialized: true,
-            cookie: {secure: false}
+            cookie: {secure: false},
+            store: new FileStore()
+
+
         })
     )
     await app.listen(3001);
