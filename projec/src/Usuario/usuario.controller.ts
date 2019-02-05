e4371d9dd8dd7fcb847f6edcdea9d6064b7ada45
davini10c <vinicio_520@hotmail.com>
2019-02-05 13:00:02 -0500

    hacienda usuario

--- projec/src/Usuario/usuario.controller.ts
+++ projec/src/Usuario/usuario.controller.ts
@@ -5,6 +5,8 @@ import {Like} from "typeorm";
 import {Region} from "../Region/region.service";
 import {RolService} from "../rol/rol.service";
 import {RolEntity} from "../rol/rol.entity";
+import {HaciendaService} from "../hacienda/hacienda.service";
+import {HaciendaEntity} from "../hacienda/hacienda.entity";
 
 @Controller('Usuario')
 
@@ -12,19 +14,25 @@ export class UsuarioController {
 
     constructor(
         private readonly __usuarioService: UsuarioService,
-        private readonly _rolesService: RolService
-     ) {
+        private readonly _rolesService: RolService,
+        private readonly _haciendaService: HaciendaService
+    ) {
 
     }
 
 
     @Get('usuario')
     async usuario(
-        @Res() response,
-        @Query('accion') accion: string,
-        @Query('nombre') nombre: string,
-        @Query('busqueda') busqueda: string,
-        @Session() sesion
+        @Res()
+            response,
+        @Query('accion')
+            accion: string,
+        @Query('nombre')
+            nombre: string,
+        @Query('busqueda')
+            busqueda: string,
+        @Session()
+            sesion
     ) {
         let mensaje; // undefined
         let clase; // undefined
@@ -67,12 +75,14 @@ export class UsuarioController {
                 ]
             };
 
-            usuarios = await this.__usuarioService.buscar(consulta);
+            usuarios = await
+            this.__usuarioService.buscar(consulta);
 
 
         }
         else {
-            usuarios = await this.__usuarioService.buscar();
+            usuarios = await
+            this.__usuarioService.buscar();
         }
         response.render('UsuarioPantalla/usuario', {
             nombreUsuario: 'Vinicio',
@@ -90,14 +100,23 @@ export class UsuarioController {
 //se inicializa la pantalla de crear usuario
     @Get('crear-usuario')
     async crearRegion(
-        @Res() response
+        @Res()
+            response
     ) {
         let roles: RolEntity[];
-        roles = await this._rolesService.buscar();
+        roles = await
+        this._rolesService.buscar();
+
+
+        let hacienda: HaciendaEntity[];
+
+        hacienda = await
+        this._haciendaService.buscar();
 
         response.render(
             'UsuarioPantalla/crear-usuario', {
                 arregloRoles: roles,
+                arregloHacienda: hacienda
 
             }
         )
@@ -106,11 +125,14 @@ export class UsuarioController {
 //CREAR USUARIO Y GUARDAR EN LA BASE DE DATOS
     @Post('crear-usuario')
     async crearRegionFormulario(
-        @Body() usuario: Usuario,
-        @Res() response
+        @Body()
+            usuario: Usuario,
+        @Res()
+            response
     ) {
 
-        await this.__usuarioService.crear(usuario);
+        await
+        this.__usuarioService.crear(usuario);
 
         const parametrosConsulta = `?accion=crear&nombre=${usuario.nombreUsuario}`;
 
@@ -122,13 +144,17 @@ export class UsuarioController {
 
     @Post('borrar/:idUsuario')
     async borrar(
-        @Param('idUsuario') idUsuario: string,
-        @Res() response
+        @Param('idUsuario')
+            idUsuario: string,
+        @Res()
+            response
     ) {
-        const usuarioEncontrada = await this.__usuarioService
+        const usuarioEncontrada = await
+        this.__usuarioService
             .buscarPorId(+idUsuario);
 
-        await this.__usuarioService.borrar(Number(idUsuario));
+        await
+        this.__usuarioService.borrar(Number(idUsuario));
 
         const parametrosConsulta = `?accion=borrar&nombre=${usuarioEncontrada.nombreUsuario}`;
 
@@ -136,14 +162,17 @@ export class UsuarioController {
     }
 
 
-    /////actualizar datos del usuario
+/////actualizar datos del usuario
 
     @Get('actualizar-usuario/:idUsuario')
     async actualizarUsuario(
-        @Param('idUsuario') idUsuario: string,
-        @Res() response
+        @Param('idUsuario')
+            idUsuario: string,
+        @Res()
+            response
     ) {
-        const usuarioAActualizar = await this
+        const usuarioAActualizar = await
+        this
             .__usuarioService
             .buscarPorId(Number(idUsuario));
 
@@ -157,13 +186,17 @@ export class UsuarioController {
 
     @Post('actualizar-usuario/:idUsuario')
     async actualizarUsuarioFormulario(
-        @Param('idUsuario') idUsuario: string,
-        @Res() response,
-        @Body() usuario: Usuario
+        @Param('idUsuario')
+            idUsuario: string,
+        @Res()
+            response,
+        @Body()
+            usuario: Usuario
     ) {
         usuario.idUsuario = +idUsuario;
 
-        await this.__usuarioService.actualizar(+idUsuario, usuario);
+        await
+        this.__usuarioService.actualizar(+idUsuario, usuario);
 
         const parametrosConsulta = `?accion=actualizar&nombre=${usuario.nombreUsuario}`;
 
